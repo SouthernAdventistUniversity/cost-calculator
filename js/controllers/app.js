aee5cbba7f6d52fa6384053f26eaabe19089ec90
Malachi Partlow <malachipartlow@gmail.com>
2017-05-08 18:11:51 -0400

    almost done

--- js/controllers/app.js
+++ js/controllers/app.js
@@ -10,7 +10,14 @@
     function AppController($scope, $http) {
 
         $scope.activeTab = 0;
-        $scope.student = {};
+        $scope.student = {
+            gpa: 2,
+            test_score: 18,
+            age: 50,
+            household_size: 11,
+            number_in_college: 6
+        };
+        $scope.states = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"];
 
         $scope.tabs = [{
                 title: 'Entering College',
@@ -40,7 +47,21 @@
                 title: 'Cost',
                 url: 'views/cost.html'
             },
-        ]
+        ];
+
+        /////////////////////////
+
+        $scope.querySearch = function(query) {
+            console.log(query)
+            if (query) {
+                var lowercaseQuery = angular.lowercase(query);
+                return $scope.states.filter(function(state) {
+                    var state = angular.lowercase(state);
+                    console.log(state.indexOf(lowercaseQuery));
+                    return (state.indexOf(lowercaseQuery) > -1)
+                });
+            } else return $scope.states;
+        }
 
     }
 })();
